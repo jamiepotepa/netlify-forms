f0ff8b3b97be92778ba76d7c9a2351be97821923
Jamie Potepa <jamie@xcetramedia.com>
2018-06-13 16:24:20 +0100

    removing recaptcha

--- src/components/Index.js
+++ src/components/Index.js
@@ -5,7 +5,7 @@ export default () => (
         <h2>Index route</h2>
 
         <div>
-           <form name="contact-form" data-netlify="true" netlify-honeypot="bot-field" method="POST" action="/success">
+           <form name="contact-form" data-netlify="true" netlify-honeypot="bot-field" method="POST">
 
                <label htmlFor="name">Name</label>
                <input type="text" name="name" id="name" placeholder="enter your name here" />
@@ -15,8 +15,6 @@ export default () => (
 
                 <label htmlFor="message">Message</label>
                 <textarea name="message" id="message" placeholder="enter your message here" />
-                
-                <div data-netlify-recaptcha></div>
 
                 <button type="submit">Send</button>
 
