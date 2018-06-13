f6ddbe26f343e02c7ef721d6eec287a9fa2fa3e6
Jamie Potepa <jamie@xcetramedia.com>
2018-06-13 21:27:07 +0100

    adding some styles to the form

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,24 +1,88 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+    width: 100;
+    max-width: 800px;
+    margin: 0 auto;
+`;
+
+const StyledForm = styled.form`
+   padding: 40px;
+   background: #f9f9f9;
+`;
+
+const FormDivide = styled.div`
+    margin-bottom: 20px;
+`;
+
+const StyledLabel = styled.label`
+    display: block;
+    font-size: 20px;
+    margin-bottom: 5px;
+`;
+
+const StyledInput = styled.input`
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    border: none;
+
+    &::-webkit-input-placeholder {
+        color: #9e9e9e;
+    }
+`;
+
+const StyledTextarea = styled.textarea`
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    border: none;
+
+     &::-webkit-input-placeholder {
+        color: #9e9e9e;
+    }
+`;
+
+const StyledButton = styled.button`
+    padding: 10px 30px;
+    border: none;
+    text-transform: uppercase;
+    font-size: 20px;
+    background: #45a2d4;
+    color: white;
+    cursor: pointer;
+`;
 
 export default () => (
-    <div>
-        <h2>Index route</h2>
+    <MainWrapper>
+        <h2>Forms with Netlify</h2>
         <div>
-           <form name="contact" data-netlify="true" netlify-honeypot="bot-field" method="POST">
+            <StyledForm name="contact" data-netlify="true" netlify-honeypot="bot-field" method="POST">
+                <input type="hidden" name="form-name" value="contact" />
+                
+                <FormDivide>
+                    <StyledLabel htmlFor="name">Name</StyledLabel>
+                    <StyledInput type="text" name="name" id="name" placeholder="enter your name here" />
+                </FormDivide>
+               
+                <FormDivide>
+                    <StyledLabel htmlFor="email">Email</StyledLabel>
+                    <StyledInput type="email" name="email" id="email" placeholder="enter your email here" />
+                </FormDivide>
 
-               <input type="hidden" name="form-name" value="contact" />
-               <label htmlFor="name">Name</label>
-               <input type="text" name="name" id="name" placeholder="enter your name here" />
+                <FormDivide>
+                    <StyledLabel htmlFor="message">Message</StyledLabel>
+                    <StyledTextarea name="message" id="message" rows="5" placeholder="enter your message here" />
+                </FormDivide>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="enter your email here" />
-
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" placeholder="enter your message here" />
                 <div data-netlify-recaptcha></div>
-                <button type="submit">Send</button>
-
-           </form>
+                
+                <FormDivide>
+                    <StyledButton type="submit">Send</StyledButton>
+                </FormDivide>
+            
+           </StyledForm>
         </div>
-    </div>
+    </MainWrapper>
 );
